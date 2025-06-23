217ee7858e0e783d836faebcce094e9fb91fd87d
Brendabaia <br3nd4lima@gmail.com>
2025-06-23 09:11:39 -0300

    aula04 construor

--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -3,6 +3,8 @@ package aula04;
 public class Caneta {
 	public String modelo;
 	private float ponta;
+	private boolean tampada;
+	
 	
 	public void Caneta() { // Método Contrutor
 		
@@ -22,6 +24,12 @@ public class Caneta {
 	
 	public void setPonta(float p) {
 		 this.ponta = p;
+	}
+	public void tampar() {
+		this.tampada = true;
+	}
+	public void destampar() {
+		this.tampada = false;
 	}
 	public void status() {
 		System.out.println("Modelo: " + this.getModelo());
