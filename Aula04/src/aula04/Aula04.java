1f9089fe28b9e2cdc4a0e4095682e5fe93e69c02
Brendabaia <br3nd4lima@gmail.com>
2025-07-25 21:42:29 -0300

    ex002

--- Aula04/src/aula04/Aula04.java
+++ Aula04/src/aula04/Aula04.java
@@ -3,7 +3,7 @@ package aula04;
 public class Aula04 {
 
 	public static void main(String[] args) {
-		Caneta c1 = new Caneta();
+		Caneta c1 = new Caneta("Bicc", "verde", 0.4f, true);
 		//c1.setModelo("BIC");
 		//c1.setPonta(0.5f);
 		c1.status();
--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -6,40 +6,55 @@ public class Caneta {
 	private float ponta;
 	private boolean tampada;
 	
-	
-	public  Caneta() { // Método Contrutor
-		this.tampar();
-		this.cor = "Azul";
+	public Caneta(String modelo, String cor, float ponta, boolean tampada) {
+		super();
+		this.modelo = modelo;
+		this.cor = cor;
+		this.ponta = ponta;
+		this.tampada = tampada;
 	}
-	
-	public String getModelo() { //Método Getters
-		return this.modelo;
+
+	public String getModelo() {
+		return modelo;
 	}
-	
-	public void setModelo(String m) { //Método Setters
-		 this.modelo = m;
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
 	}
-	
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
 	public float getPonta() {
-		return this.ponta;
+		return ponta;
 	}
-	
-	public void setPonta(float p) {
-		 this.ponta = p;
+
+	public void setPonta(float ponta) {
+		this.ponta = ponta;
 	}
-	public void tampar() {
-		this.tampada = true;
+
+	public boolean isTampada() {
+		return tampada;
 	}
-	public void destampar() {
-		this.tampada = false;
+
+	public void setTampada(boolean tampada) {
+		this.tampada = tampada;
 	}
 	public void status() {
-		System.out.println("SOBRE A CANETA:");
+			System.out.println("SOBRE A CANETA:");
 		System.out.println("Modelo: " + this.getModelo());
 		System.out.println("Ponta: " + this.getPonta());
 		System.out.println("Cor: " + this.cor);
 		System.out.println("Tampada: " + this.tampada);
+		
 	}
+	
+	
 	
 
 }
